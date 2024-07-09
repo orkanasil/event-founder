f5cd3a1119512d97e474f09345e1108f8cfa0449
orkan asil <orkancaprak@outlook.com>
2024-07-09 16:03:35 +0300

    Update: Sidebar

---
+++ src/components/local/AppSidebar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex flex-col gap-6 pl-16 pt-12 text-xl font-light">
+    <h1 class="text-3xl font-bold">BRAND</h1>
+    <div class="flex flex-col gap-6 pt-8">
+      <a href="">
+        <i class="fa-sharp fa-solid fa-house fa-sm pr-2" style="color: #929db0"></i>
+        Home
+      </a>
+      <a href="">
+        <i class="fa-sharp fa-solid fa-heart fa-sm pr-2" style="color: #929db0"></i>
+        Favorities
+      </a>
+      <a href="">
+        <i
+          class="fa-sharp fa-solid fa-circle-question fa-sm pr-2"
+          style="color: #929db0"
+        ></i>
+        About
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="flex h-screen justify-center bg-sky-200">
-    <div class="container mt-40 h-2/4 w-3/4 rounded-2xl bg-white"></div>
+    <div class="container mt-40 h-2/4 w-3/4 rounded-2xl bg-white">
+      <div class="w-64 h-full bg-sky-50 rounded-l-2xl">
+        <AppSidebar />
+      </div>
+      <div></div>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import AppSidebar from "@/components/local/AppSidebar.vue";
+</script>
 
 <style scoped></style>
