886e81bb1f7277d7351d2f5ed641b71cd5ce8411
orkan asil <orkancaprak@outlook.com>
2024-07-22 19:30:41 +0300

    Update: Aboute Page

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import App from "./App.vue";
 import "./plugins/axios";
 import installGlobalComponents from "./plugins/globalComponents";
 import router from "./router/index";
+import { PaginationBar } from "v-page";
 
 const app = createApp(App);
 
 installGlobalComponents(app);
 
+app.use(PaginationBar);
 app.use(createPinia());
 app.use(router);
 app.mount("#app");
--- src/views/About/AboutEvent.vue
+++ src/views/About/AboutEvent.vue
@@ -1,9 +1,36 @@
 <template>
   <div>
-    <p>About</p>
+    <div v-for="item in listGallery" :key="item" v-text="item" />
   </div>
+  <PaginationBar
+    class="h-8"
+    :first="false"
+    :last="false"
+    :total-row="30"
+    :language="lang"
+    :page-size-options="false"
+    :info="false"
+    :disabled="false"
+    align="left"
+    @change="changeGallery"
+  />
 </template>
+<script setup>
+import { ref } from "vue";
+import { PaginationBar } from "v-page";
 
-<script setup></script>
+const srcList = Array(88)
+  .fill(0)
+  .map((val, index) => index + 1);
+const listGallery = ref([]);
 
-<style lang="scss" scoped></style>
+function changeGallery({ pageNumber, pageSize }) {
+  const start = pageSize * (pageNumber - 1);
+  const end =
+    start + pageSize > srcList.length ? srcList.length : start + pageSize;
+
+  listGallery.value = srcList.filter((val, index) => {
+    return index >= start && index < end;
+  });
+}
+</script>
