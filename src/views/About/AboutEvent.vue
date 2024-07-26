ea2982b80376e587b66bec23a3ca2beb947a5077
orkan asil <orkancaprak@outlook.com>
2024-07-26 13:26:40 +0300

    Update: About page content

--- src/views/About/AboutEvent.vue
+++ src/views/About/AboutEvent.vue
@@ -1,36 +1,33 @@
 <template>
-  <div>
-    <div v-for="item in listGallery" :key="item" v-text="item" />
+  <div class="mb-10 w-full rounded-2xl bg-sky-50 px-10 py-16">
+    <div class="flex max-w-[800px] flex-col gap-6 text-slate-500">
+      <p>
+        Welcome to Event Founder, your ultimate destination for discovering the
+        most exciting music and sports events happening around you. Whether
+        you're a fan of live concerts, thrilling sports matches, or looking for
+        new experiences to enjoy with friends and family, Event Founder has got
+        you covered. Our app is designed to help you find and attend the best
+        events with ease and convenience.
+      </p>
+      <p>
+        At Event Founder, we believe in the power of shared experiences. Our
+        mission is to connect people with the events they love, creating
+        unforgettable memories along the way. With our user-friendly interface,
+        you can easily browse through a wide range of events, get detailed
+        information, and even purchase tickets directly through the app. We
+        strive to bring the latest and most popular events to your fingertips,
+        making it simple for you to stay updated and never miss out on the
+        action.
+      </p>
+      <p>
+        Our team is dedicated to providing you with a seamless and enjoyable
+        event discovery experience. We continuously update our event listings to
+        ensure you have access to the most current information. Whether you're
+        looking for local gigs, major league sports events, or international
+        music festivals, Event Founder is here to guide you every step of the
+        way. Join our community of event enthusiasts today and start exploring
+        the vibrant world of entertainment with Event Founder!
+      </p>
+    </div>
   </div>
-  <PaginationBar
-    class="h-8"
-    :first="false"
-    :last="false"
-    :total-row="30"
-    :language="lang"
-    :page-size-options="false"
-    :info="false"
-    :disabled="false"
-    align="left"
-    @change="changeGallery"
-  />
 </template>
-<script setup>
-import { ref } from "vue";
-import { PaginationBar } from "v-page";
-
-const srcList = Array(88)
-  .fill(0)
-  .map((val, index) => index + 1);
-const listGallery = ref([]);
-
-function changeGallery({ pageNumber, pageSize }) {
-  const start = pageSize * (pageNumber - 1);
-  const end =
-    start + pageSize > srcList.length ? srcList.length : start + pageSize;
-
-  listGallery.value = srcList.filter((val, index) => {
-    return index >= start && index < end;
-  });
-}
-</script>
